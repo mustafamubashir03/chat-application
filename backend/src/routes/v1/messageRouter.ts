7580e3be9b1f6d078fdcf6ad1730c5b48a5d3190
unknown <mustafamubashir87@gmail.com>
2025-09-27 02:23:04 +0500

    Added separate route for getting  messages

--- backend/src/routes/v1/messageRouter.ts
+++ backend/src/routes/v1/messageRouter.ts
@@ -5,6 +5,6 @@ import { isAuthenticated } from '../../middlewares/isAuthenticated';
 
 const router = express.Router();
 
-router.post('/messages/:channelId', isAuthenticated, getMessagesController);
+router.get('/:channelId', isAuthenticated, getMessagesController);
 
 export default router;
